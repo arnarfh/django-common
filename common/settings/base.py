74515262bdb41d9c7c0a54c2a36313b83611008b
Arnar Freyr Helgason <nozagleh@gmail.com>
2019-07-21 14:20:31 -0500

    Add GA_CODE to context processor

--- common/settings/base.py
+++ common/settings/base.py
@@ -83,7 +83,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'common.context_processors.add_env_to_context'
+                'common.context_processors.add_env_to_context',
+                'common.context_processors.add_ga_code_to_context',
             ],
         },
     },
