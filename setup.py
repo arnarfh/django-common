d00c504d1c60708fb186670507170419867d42de
Arnar Freyr Helgason <nozagleh@gmail.com>
2019-08-05 13:56:48 -0500

    Increase version of django-common package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-common',
-      version='1.9',
+      version='1.9.1',
       description='Django common package',
       packages=find_packages(),
      )
