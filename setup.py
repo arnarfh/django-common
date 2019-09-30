9d6c0c1ee2d33a463a02e319ec9b90f7013b1dbe
Arnar Freyr Helgason <nozagleh@gmail.com>
2019-09-30 19:59:28 +0000

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-common',
-      version='1.9.7',
+      version='1.9.8',
       description='Django common package',
       packages=find_packages(),
      )
