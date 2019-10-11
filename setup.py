7d4a85faf847aca0ad1a5adc93347176fd3293b0
Arnar Freyr Helgason <nozagleh@gmail.com>
2019-10-11 01:17:26 +0000

    Fix error in context processor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-common',
-      version='2.0.4',
+      version='2.0.5',
       description='Django common package',
       packages=find_packages(),
      )
