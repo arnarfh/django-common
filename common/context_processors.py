def02a6b081f766002823763d7fc9d9014768280
Arnar Freyr Helgason <nozagleh@gmail.com>
2019-10-11 01:07:48 +0000

    Context processor tweaks

--- common/context_processors.py
+++ common/context_processors.py
@@ -10,11 +10,12 @@ def add_ga_code_to_context(request):
     return {"ga_code": GA_CODE}
 
 def add_static_endpoint_to_context(request):
+    end = None
     if os.getenv("ENV") =! "production":
         from project.settings.production import AWS_STORAGE_BUCKET_NAME, DO_SPACE_ENDPOINT
+        end = "https://" + AWS_STORAGE_BUCKET_NAME + "." + DO_SPACE_ENDPOINT + "/"
     if os.getenv("ENV") == "staging":
         from project.settings.staging import AWS_STORAGE_BUCKET_NAME, DO_SPACE_ENDPOINT
-        
-    end = "https://" + AWS_STORAGE_BUCKET_NAME + "." + DO_SPACE_ENDPOINT + "/"
+        end = "https://" + AWS_STORAGE_BUCKET_NAME + "." + DO_SPACE_ENDPOINT + "/"
     
     return {"static_files_public": end}
