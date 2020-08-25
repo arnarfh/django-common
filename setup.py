9d537cda04b12dc23cc628b3172c9f19b8eee00c
Arnar Freyr Helgason <nozagleh@gmail.com>
2020-08-25 17:04:58 +0000

    Don't require image caption

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-common',
-      version='2.0.7',
+      version='2.0.8',
       description='Django common package',
       packages=find_packages(),
      )
